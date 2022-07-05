dbd047ea545d53dda94af3471b2f5f4cd072f5fa
Diego Farias <42777378+diegosfarias@users.noreply.github.com>
2022-07-05 10:50:20 -0400

    Create my_python_file_1.py

---
+++ my_python_file_1.py
@@ -0,0 +1,12 @@
+from pypi_install import pypi_install as PI
+pypirc_username="my_pypi_username" # for e.g. "diegosfarias"
+pypirc_password= "my_pypi_password" # for e.g. "09Junhos"
+directory_of_new_folder = "path_new_directory" # for e.g. "/home/USER/New_Folder"
+version_number="your_project_version_no" # for e.g. "0.0.2"
+author_name_full="full_author_name" # for e.g. "John Smith"
+author_email="author_email" # for e.g. johnsmith123@hotmail.com
+short_description="short_description" # for e.g. "My first PyPi project"
+github_url="your_code_github_url" # for e.g. https://github.com/diegosfarias/pypiconda
+python_version="python_version_in_your_computer" for e.g. "3" or "2"
+name_of_project="name_of_pypi_project" for e.g. "my_pypi_code"
+directory_of_python_files=["PATH/my_python_file_1.py","PATH/my_python_file_2.py"] # Include all the python files here
